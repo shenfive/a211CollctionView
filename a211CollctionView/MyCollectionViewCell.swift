f69b00a07cd927f430354bab4f35293162c87a7d
Shen Jun Wu <shenfive@gmail.com>
2020-12-05 11:12:13 +0800

    fill Cell

---
+++ a211CollctionView/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  a211CollctionView
+//
+//  Created by 申潤五 on 2020/12/5.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var theImageView: UIImageView!
+    
+}
--- a211CollctionView/ViewController.swift
+++ a211CollctionView/ViewController.swift
@@ -11,20 +11,31 @@ class ViewController: UIViewController,UICollectionViewDataSource {
 
 
     @IBOutlet weak var myCollectionView: UICollectionView!
+    var images:[UIImage?] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        images = [ UIImage(named: "image1"),
+                   UIImage(named: "image2"),
+                   UIImage(named: "image3"),
+                   UIImage(named: "image4"),
+                   UIImage(named: "image5")
+        ]
         myCollectionView.dataSource = self
     }
 
     
     //MARK: UIColltionView DataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
-        cell.backgroundColor = UIColor.red
+                    as! MyCollectionViewCell 
+        cell.theImageView.image = images[indexPath.row]
         return cell
     }
     
