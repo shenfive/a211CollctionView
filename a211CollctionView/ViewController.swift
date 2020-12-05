77dd77932eff09964209ed716e7de33d1e8357a3
Shen Jun Wu <shenfive@gmail.com>
2020-12-05 11:44:54 +0800

    animated layout

--- a211CollctionView/ViewController.swift
+++ a211CollctionView/ViewController.swift
@@ -26,23 +26,55 @@ class ViewController: UIViewController,UICollectionViewDataSource {
         myCollectionView.dataSource = self
         
         let screenSize = UIScreen.main.bounds.size
-        
         let layout = UICollectionViewFlowLayout()
-        
         // 設置 section 的間距 四個數值分別代表 上、左、下、右 的間距
-        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-        
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         // 設置每一行的間距
-        layout.minimumLineSpacing = 5
-        
+        layout.minimumLineSpacing = 10
         // 設置每個 cell 的尺寸
-        layout.itemSize = CGSize(width: (screenSize.width / 3 - 10.0), height: screenSize.width / 3 - 10.0)
+        layout.itemSize = CGSize(width: (screenSize.width / 3 - 10), height: screenSize.width / 3 - 10 )
 
         myCollectionView.setCollectionViewLayout(layout, animated: true)
         
         
     }
 
+    
+    
+    @IBAction func segAction(_ sender: UISegmentedControl) {
+        var itemNumberInCell:CGFloat = 3
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            itemNumberInCell = 3
+        case 1:
+            itemNumberInCell = 4
+        case 2:
+            itemNumberInCell = 5
+        default:
+            break
+        }
+        
+        
+        
+        
+        let screenSize = UIScreen.main.bounds.size
+        let layout = UICollectionViewFlowLayout()
+        // 設置 section 的間距 四個數值分別代表 上、左、下、右 的間距
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        // 設置每一行的間距
+        layout.minimumLineSpacing = 10
+        // 設置每個 cell 的尺寸
+        layout.itemSize = CGSize(width: (screenSize.width / itemNumberInCell - 10), height: screenSize.width / itemNumberInCell - 10 )
+
+        myCollectionView.setCollectionViewLayout(layout, animated: true)
+        
+    }
+    
+    
+    
+    
+    
     
     //MARK: UIColltionView DataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
