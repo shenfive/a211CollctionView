159cd1537e135e4108625090c5b28e3f25852996
Shen Jun Wu <shenfive@gmail.com>
2020-12-05 10:21:10 +0800

    init

--- a211CollctionView/ViewController.swift
+++ a211CollctionView/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource {
 
+
+    @IBOutlet weak var myCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myCollectionView.dataSource = self
     }
 
-
+    
+    //MARK: UIColltionView DataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
 }
 
